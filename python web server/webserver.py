e17788056ad0a2b8407b4aab5842c754e8e43624
CarlosAlfredo <56643645+Carlos-Alfredo@users.noreply.github.com>
2022-07-11 09:08:16 -0300

    Add files via upload

--- python web server/webserver.py
+++ python web server/webserver.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 import io
 import json
 import logging
-from PIL import Image
 from pymongo import MongoClient
 import base64
+import boto3
 
-mongodbIp='54.221.150.139'
+def get_mongoIP():
+	client=boto3.client('ec2',
+	region_name= "us-east-1",
+    aws_access_key_id="AKIAV5MQB4Y6QUV5XP6W",
+    aws_secret_access_key="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")
+	reservations = client.describe_instances(InstanceIds=['i-00e60cdceba35d872']).get("Reservations")
+	for reservation in reservations:
+		for instance in reservation['Instances']:
+			return(instance.get("PublicIpAddress"))
+
+mongodbIp=get_mongoIP()
 
 # Logger settings - CloudWatch
 logger = logging.getLogger()
@@ -17,13 +27,14 @@ client = MongoClient('mongodb://{}:27017/'.format(mongodbIp))
 
 # Set database
 #db = client.camera_images
-db = client.test_database
+db = client.cameraImage
 users = db.users
 
 app = Flask(__name__)
+app.secret_key='123'
 
 @app.route("/")
-def index():
+def start():
 	return render_template('index.html')
 
 @app.route('/showImage/', methods = ['POST','GET'])
@@ -32,11 +43,40 @@ def showImage():
 		return f"The URL /showImage is accessed directly. Try going to '/index' to submit form"
 	if request.method == 'POST':
 		data = request.form
-		image = users.find_one({'cameraId':data['Camera_Id']})
+		session['Camera_Id']=data['Camera_Id']
+		results = users.find({'cameraId':session['Camera_Id']}).limit(1).sort('timestamp',-1)
+		print(results)
+		for x in results:
+			image=x
+		print(image)
 		img_buffer=io.BytesIO(image['data'])
 		img_buffer.seek(0)
 		image_memory = base64.b64encode(img_buffer.getvalue())
 		return render_template('showImage.html', camera_image = image_memory.decode('utf-8'))
+
+@app.route('/showImage/updateImage/')
+def redirectUpImg1():
+	return redirect('/updateImage/')
+
+@app.route('/updateImage/updateImage/')
+def redirectUpImg2():
+	return redirect('/updateImage/')
+
+@app.route('/showImage/index/')
+def redirectIndex1():
+	return redirect('/')
+
+@app.route('/updateImage/index/')
+def redirectIndex2():
+	return redirect('/')
+
+@app.route('/updateImage/')
+def updateImage():
+	image = users.find_one({'cameraId':session['Camera_Id']})
+	img_buffer=io.BytesIO(image['data'])
+	img_buffer.seek(0)
+	image_memory = base64.b64encode(img_buffer.getvalue())
+	return render_template('showImage.html', camera_image = image_memory.decode('utf-8'))
 
 if __name__ == '__main__':
    app.run(host="0.0.0.0", port=80)
