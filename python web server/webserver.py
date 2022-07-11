8ee532dfb4d27dfe68f604e5eb5aee8c1cf561aa
CarlosAlfredo <56643645+Carlos-Alfredo@users.noreply.github.com>
2022-07-10 21:11:58 -0300

    Add files via upload

--- python web server/webserver.py
+++ python web server/webserver.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import io
+import json
+import logging
+from PIL import Image
+from pymongo import MongoClient
+import base64
+
+mongodbIp='54.221.150.139'
+
+# Logger settings - CloudWatch
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Set client
+client = MongoClient('mongodb://{}:27017/'.format(mongodbIp))
+
+# Set database
+#db = client.camera_images
+db = client.test_database
+users = db.users
+
 app = Flask(__name__)
 
-#@app.route("/", methods=['GET', 'POST'])
 @app.route("/")
 def index():
-   return render_template('index.html')
+	return render_template('index.html')
+
+@app.route('/showImage/', methods = ['POST','GET'])
+def showImage():
+	if request.method == 'GET':
+		return f"The URL /showImage is accessed directly. Try going to '/index' to submit form"
+	if request.method == 'POST':
+		data = request.form
+		image = users.find_one({'cameraId':data['Camera_Id']})
+		img_buffer=io.BytesIO(image['data'])
+		img_buffer.seek(0)
+		image_memory = base64.b64encode(img_buffer.getvalue())
+		return render_template('showImage.html', camera_image = image_memory.decode('utf-8'))
 
 if __name__ == '__main__':
-   app.run(debug=True)
+   app.run(host="0.0.0.0", port=80)
